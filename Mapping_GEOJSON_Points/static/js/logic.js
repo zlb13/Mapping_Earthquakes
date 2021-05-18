2f57d42f2ecf43396b54926c945f3fa138dd2c73
Z Browning <zybrowning@yahoo.com>
2021-05-18 00:53:17 -0400

    datafile

--- Mapping_GEOJSON_Points/static/js/logic.js
+++ Mapping_GEOJSON_Points/static/js/logic.js
@@ -4,44 +4,10 @@ console.log("working");
 // Create the map object with center at the San Francisco airport.
 let map = L.map("mapid", {
     center: [
-        37.5, -122.5
+        30, 30
     ],
-    zoom: 10
+    zoom: 2
   });
-
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
-
-
-// Grabbing our GeoJSON data.
-L.geoJson(sanFranAirport, {
-    // We turn each feature into a marker on the map.
-    onEachFeature: function(feature, layer) {
-        console.log(layer);
-        layer.bindPopup("<h2>" + feature.properties.name + "</h2><hr><p>" +  feature.properties.faa + "<p>");
-       
-    }
-    
-
-  }).addTo(map);
-
 
 
 // We create the tile layer that will be the background of our map.
@@ -52,3 +18,20 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-str
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/https://raw.githubusercontent.com/zlb13/Mapping_Earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data, {
+      onEeachFeature: function(feature, layer) {
+          console.log(layer);
+          layer.bindPopup("<h2>" + feature.properties.name + "</h2>").addTo(map);
+      }
+      
+  })
+  
+});
